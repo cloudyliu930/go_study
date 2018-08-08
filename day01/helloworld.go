8ad8867b9b1849d6a861a208e532b99dd883572f
cloudyliu <200537930>
2018-08-08 23:23:19 +0800

    提交常用语法测试

--- day01/helloworld.go
+++ day01/helloworld.go
@@ -2,10 +2,29 @@ package main
 
 import "fmt"
 
+var (
+	aa = 3
+	bb = 4
+	cc = 5
+)
+
+func test1() {
+	a := 1
+	b := 2
+	fmt.Println(a, b)
+}
+
+func test2() {
+	fmt.Println(aa, bb, cc)
+}
+
 func main() {
 	fmt.Println("hello world!!!!")
 
 	var a int = 4
 	var str string = "abc"
 	fmt.Print(a, str)
+
+	test1()
+	test2()
 }
